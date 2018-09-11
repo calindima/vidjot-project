2827239dce7644b3c3256612f8911ac447f804b4
calindima <calin.dima@yahoo.com>
2018-09-11 13:03:36 +0200

    2nd commit.mispelled mlab user and password

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV === 'production'){
   module.exports = {
-    mongoURI: 'mongodb://<calin>:<calin123>@ds251332.mlab.com:51332/vidjot-prod'
+    mongoURI: 'mongodb://calin:calin123@ds251332.mlab.com:51332/vidjot-prod'
   }
 } else {
 module.exports = {
